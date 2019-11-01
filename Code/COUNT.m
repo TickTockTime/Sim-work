9035e9a8509f3778230f7f431b03625da03923fb
Tardis <3160102438@zju.edu.cn>
2019-11-01 16:16:09 +0800

    Finish Basic Functions

--- Code/COUNT.m
+++ Code/COUNT.m
@@ -5,17 +5,19 @@
 @:return ES:        节点成功交互次数
 @:return EA:        节点交互总数
 %}
-function [A, S, SC]=COUNT(Req, Slevel, node, A, S, SC)
+function [A, S, SA, SC]=COUNT(Req, Slevel, node, A, SA, SC)
 
 num=size(Req,2);
+S=zeros(1, num);
 for i=1:num
     if i~=node && Req(1, i)~=0
         A(1,i)=A(1,i)+1;
         if  Req(1,i)<=Slevel
-            S(1, i)=S(1,i)+1;
+            S(1, i)=1;
+            SA(1, i)=SA(1, i)+1;
             SC(1,i)=SC(1,i)+1;
         else
-%             S(1, i)=0;
+            S(1, i)=0;
             SC(1,i)=0;
         end
     end
--- Code/main.m
+++ Code/main.m
@@ -41,6 +41,7 @@ for i=1:N1
     Elayer(i).Rev=zeros(1, Elayer(i).NerNode);
     Elayer(i).Ns=zeros(1, Elayer(i).NerNode);
     Elayer(i).Na=zeros(1, Elayer(i).NerNode);
+    Elayer(i).Nsa=zeros(1, Elayer(i).NerNode);
     Elayer(i).Nsc=zeros(1, Elayer(i).NerNode);
     Elayer(i).T=Elayer(i).trust*ones(1, Elayer(i).NerNode);
     Elayer(i).Slevel=CONTROL(Elayer(i).trust);
@@ -60,6 +61,7 @@ for i=1:N2
     Mlayer(i).Rev=zeros(1, Mlayer(i).NerNode);
     Mlayer(i).Ns=zeros(1, Mlayer(i).NerNode);
     Mlayer(i).Na=zeros(1, Mlayer(i).NerNode);
+    Mlayer(i).Nsa=zeros(1, Mlayer(i).NerNode);
     Mlayer(i).Nsc=zeros(1, Mlayer(i).NerNode);
     Mlayer(i).T=Mlayer(i).trust*ones(1, Mlayer(i).NerNode);
     Mlayer(i).Slevel=CONTROL(Mlayer(i).trust);
@@ -73,6 +75,7 @@ for i=1:N3
     Flayer(i).Rev=zeros(1, Flayer(i).NerNode);
     Flayer(i).Ns=zeros(1, Flayer(i).NerNode);
     Flayer(i).Na=zeros(1, Flayer(i).NerNode);
+    Flayer(i).Nsa=zeros(1, Flayer(i).NerNode);
     Flayer(i).Nsc=zeros(1, Flayer(i).NerNode);
     Flayer(i).T=Flayer(i).trust*ones(1, Flayer(i).NerNode);
     Flayer(i).Slevel=CONTROL(Flayer(i).trust);
@@ -90,10 +93,35 @@ Keynode4=zeros(1,Times+1); Keynode4(1,1)=Trust0;
 for t=1:Times
     for i=1:N1
         Elayer(i).Slevel=CONTROL(Elayer(i).trust);
+%         所有相关节点都产生请求信号
 %         Elayer(i).Req=ones(1,Elayer(i).NerNode);
+        % 随机产生请求信号
         Elayer(i).Req=randi([0,3],1,Elayer(i).NerNode);
-        [Elayer(i).Na, Elayer(i).Ns, Elayer(i).Nsc]=COUNT(Elayer(i).Req, Elayer(i).Slevel, Elayer(i).id, Elayer(i).Na,Elayer(i).Ns, Elayer(i).Nsc);
-        Elayer(i).T=TRUST(alpha0,beta0,Elayer(i).Ns,Elayer(i).Na,Elayer(i).Nsc,Thn,(sum(Elayer(i).T(1,1:N1))-Elayer(i).T(i))/(N1-1),Elayer(i).id);
+        [Elayer(i).Na, Elayer(i).Ns, Elayer(i).Nsa, Elayer(i).Nsc]=...
+            COUNT(Elayer(i).Req, Elayer(i).Slevel, Elayer(i).id, Elayer(i).Na,Elayer(i).Nsa, Elayer(i).Nsc);
+        
+        % 对每个成功请求，填入对应节点的接收内容
+        for j=1:N1
+            if Elayer(i).Ns(j)==1
+                Elayer(j).Rev(i)=1;
+            else
+                Elayer(j).Rev(i)=0;
+            end
+        end
+        
+        if ismember(i, EtoM)
+            for j=1:size(Mreceive, 2)
+                if Elayer(i).Ns(N1+j)==1
+                    Mlayer(Mreceive(j)).Rev(N2+find(EtoM==i))=1;
+                else
+                    Mlayer(Mreceive(j)).Rev(N2+find(EtoM==i))=0;
+                end
+            end
+        end
+        
+        % 两个节点之间进行信任值计算
+        Elayer(i).T=TRUST(alpha0,beta0,Elayer(i).Nsa,Elayer(i).Na,Elayer(i).Nsc,Thn,(sum(Elayer(i).T(1,1:N1))-Elayer(i).T(i))/(N1-1),Elayer(i).id);
+        % 平均求出综合信任值
         Elayer(i).trust=(sum(Elayer(i).T)-Elayer(i).T(i))/(Elayer(i).NerNode-1);
     end
     
@@ -102,25 +130,86 @@ for t=1:Times
         Mlayer(i).Slevel=CONTROL(Mlayer(i).trust);
 %         Mlayer(i).Req=ones(1,Mlayer(i).NerNode);
         Mlayer(i).Req=randi([0,3],1,Mlayer(i).NerNode);
-        [Mlayer(i).Na, Mlayer(i).Ns, Mlayer(i).Nsc]=COUNT(Mlayer(i).Req, Mlayer(i).Slevel, Mlayer(i).id, Mlayer(i).Na,Mlayer(i).Ns, Mlayer(i).Nsc);
-        Mlayer(i).T=TRUST(alpha0,beta0,Mlayer(i).Ns,Mlayer(i).Na,Mlayer(i).Nsc,Thn,(sum(Mlayer(i).T(1,1:N2))-Mlayer(i).T(i))/(N2-1),Mlayer(i).id);
+        [Mlayer(i).Na, Mlayer(i).Ns, Mlayer(i).Nsa, Mlayer(i).Nsc]=...
+            COUNT(Mlayer(i).Req, Mlayer(i).Slevel, Mlayer(i).id, Mlayer(i).Na,Mlayer(i).Nsa, Mlayer(i).Nsc);
+        
+        for j=1:N2
+            if Mlayer(i).Ns(j)==1
+                Mlayer(j).Rev(i)=1;
+            else
+                Mlayer(j).Rev(i)=0;
+            end
+        end
+        
+        if ismember(i, Mreceive)
+            for j=1:size(EtoM, 2)
+                if Mlayer(i).Ns(N2+j)==1
+                    Elayer(EtoM(j)).Rev(N1+find(Mreceive==i))=1;
+                else
+                    Elayer(EtoM(j)).Rev(N1+find(Mreceive==i))=0;
+                end
+            end
+        end
+        
+        if ismember(i, MtoF)
+            for j=1:N3/2
+                if Mlayer(i).Ns(N2+j)==1
+                    Flayer(N3/2*(find(MtoF==i)-1)+j).Rev=1;
+                else
+                    Flayer(N3/2*(find(MtoF==i)-1)+j).Rev=0;
+                end
+            end
+        end
+        
+        Mlayer(i).T=TRUST(alpha0,beta0,Mlayer(i).Nsa,Mlayer(i).Na,Mlayer(i).Nsc,Thn,(sum(Mlayer(i).T(1,1:N2))-Mlayer(i).T(i))/(N2-1),Mlayer(i).id);
         Mlayer(i).trust=(sum(Mlayer(i).T)-Mlayer(i).T(i))/(Mlayer(i).NerNode-1);
     end
     
     for i=1:N3
         Flayer(i).Slevel=CONTROL(Flayer(i).trust);
-%         Mlayer(i).Req=ones(1,Mlayer(i).NerNode);
+%         Flayer(i).Req=ones(1,Flayer(i).NerNode);
         Flayer(i).Req=randi([0,3],1,Flayer(i).NerNode);
-        [Flayer(i).Na, Flayer(i).Ns, Flayer(i).Nsc]=COUNT(Flayer(i).Req, Flayer(i).Slevel, Flayer(i).id, Flayer(i).Na,Flayer(i).Ns, Flayer(i).Nsc);
-        Flayer(i).T=TRUST(alpha0,beta0,Flayer(i).Ns,Flayer(i).Na,Flayer(i).Nsc,Thn,Flayer(i).trust,Flayer(i).id);
+        if Flayer(i).Req~=0
+            Flayer(i).Na=Flayer(i).Na+1;
+            if Flayer(i).Req<=Flayer(i).Slevel
+                Flayer(i).Ns=1;
+                Flayer(i).Nsa=Flayer(i).Nsa+1;
+                Flayer(i).Nsc=Flayer(i).Nsc+1;
+            else
+                Flayer(i).Ns=0;
+                Flayer(i).Nsc=0;
+            end
+        else
+            Flayer(i).Ns=0;
+        end
+        
+        if i<=N3/2
+            if Flayer(i).Ns==1
+                Mlayer(MtoF(1)).Rev(N2+i)=1;
+            else
+                Mlayer(MtoF(1)).Rev(N2+i)=0;
+            end
+        else
+            if Flayer(i).Ns==1
+                Mlayer(MtoF(2)).Rev(N2+i-5)=1;
+            else
+                Mlayer(MtoF(2)).Rev(N2+i-5)=1;
+            end
+        end
+
+        Flayer(i).T=TRUST(alpha0,beta0,Flayer(i).Nsa,Flayer(i).Na,Flayer(i).Nsc,Thn,Flayer(i).trust,0);
         Flayer(i).trust=Flayer(i).T;
     end
     
     Keynode1(1, t+1)=Elayer(2).trust;
     Keynode2(1, t+1)=Elayer(3).trust;
     Keynode3(1, t+1)=Mlayer(4).trust;
-    Keynode4(1, t+1)=Flayer(2).trust;
+    Keynode4(1, t+1)=Flayer(1).trust;
 end
+
+
+
+%% Plot function
 temp=0:Times;
 subplot(4,1,1);
 plot(temp,Keynode1,'linewidth',1.5);
